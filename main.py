9f613cd17f8cc9026326724ed43c74c9907442f0
Alexander <awaumans@me.com>
2024-11-23 09:20:19 +0100

    def download

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import os
 command = "zowe zos-files download ds Z58582.SYSINFO"
 sysinfo_file_path = "z58582/sysinfo.txt"
 
-def check_age_file(file_path):
+def download_sysfile(file_path):
     if os.path.exists(file_path):
-        age_file = os.path.getmtime(file_path)
-
-        if age_file > 3600:
-            subprocess(command, )
+        print("file exists")
+    else:
+        print(f"file in ${sysinfo_file_path} not found, downloading")
+        subprocess.run(command, shell=True, capture_output=True)
     
 
 check_age_file(sysinfo_file_path)
